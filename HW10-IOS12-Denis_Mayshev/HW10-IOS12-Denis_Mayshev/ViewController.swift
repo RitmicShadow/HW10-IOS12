46831259b9b1e5461474d6c21409476accd2e1bc
DenisMayshev <denmay13@mail.ru>
2023-12-17 13:52:57 +0400

    feture

---
+++ HW10-IOS12-Denis_Mayshev/HW10-IOS12-Denis_Mayshev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW10-IOS12-Denis_Mayshev
+//
+//  Created by Ден Майшев on 17.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
